ada2ee7d1a315d4a537bcc692edc40925f05c78b
Jennie Van <missjenvan@gmail.com>
2019-12-16 17:11:50 -0800

    added user model and router

--- api/server.js
+++ api/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 
 const server = express();
+const apiRouter = require('./api-router.js');
 
 server.use(express.json());
+
+server.use('/api', apiRouter);
 
 
 module.exports = server;
--- users/users-router.js
+++ users/users-router.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
-const Uer = require('./users-model.js');
+const Users = require('./users-model.js');
 
 router.get('/', (req, res) => {
-    
+    Users.find()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(err => {
+            res.send(err);
+        })
 });
 
 module.exports = router;
